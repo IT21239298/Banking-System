3e5372ef195194c8ed48225700dd8a06c94baea6
IT21239298 <it21239298@my.sliit.lk>
2023-08-02 18:53:22 +0530

    add img

---
+++ src/bank/Login.java
@@ -0,0 +1,14 @@
+package bank;
+
+import javax.swing.*;
+
+public class Login  extends JFrame {
+
+    Login(){
+
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
