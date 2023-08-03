595569a5ca12dbe2d84f18be69e88761d0e87292
IT21239298 <it21239298@my.sliit.lk>
2023-08-04 01:30:15 +0530

    add calender

--- src/bank/Signup.java
+++ src/bank/Signup.java
@@ -1,10 +1,16 @@
 package bank;
 
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
 public class Signup extends JFrame {
+
+    JTextField textName,textFname;
+
+    JDateChooser dateChooser;
 
     Random ran = new Random();
     long first4 = (ran.nextLong() % 9000L) + 1000L;
@@ -24,6 +30,53 @@ public class Signup extends JFrame {
         label1.setBounds(160,20,600,40);
         label1.setFont(new Font("Railway",Font.BOLD,38));
         add(label1);
+
+        JLabel label2 = new JLabel("Page 1");
+        label2.setFont(new Font("Railway",Font.BOLD,22));
+        label2.setBounds(330,70,600,30);
+        add(label2);
+
+        JLabel label3 = new JLabel("Personal Details");
+        label3.setFont(new Font("Railway",Font.BOLD,22));
+        label3.setBounds(290,90,600,30);
+        add(label3);
+
+        JLabel labelName = new JLabel("Name :");
+        labelName.setFont(new Font("Railway", Font.BOLD,20));
+        labelName.setBounds(100,190,100,30);
+        add(labelName);
+
+        textName = new JTextField();
+        textName.setFont(new Font("Railway",Font.BOLD,14));
+        textName.setBounds(300,190,400,30);
+        add(textName);
+
+        JLabel labelfName = new JLabel("Father's Name :");
+        labelfName.setFont(new Font("Railway", Font.BOLD,20));
+        labelfName.setBounds(100,240,200,30);
+        add(labelfName);
+
+        textFname = new JTextField();
+        textFname.setFont(new Font("Railway",Font.BOLD,14));
+        textFname.setBounds(300,240,400,30);
+        add(textFname);
+
+        JLabel DOB = new JLabel("Date of Birth :");
+        DOB.setFont(new Font("Railway", Font.BOLD,20));
+        DOB.setBounds(100,340,200,30);
+        add(DOB);
+
+        dateChooser = new JDateChooser();
+        dateChooser.setForeground(new Color(105,105,105));
+        dateChooser.setBounds(300,340,400,30);
+        add(dateChooser);
+
+
+
+
+
+
+
 
 
 
