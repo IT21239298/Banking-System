52c111eb23bebe61e062f0197a468bbb22a315ce
IT21239298 <it21239298@my.sliit.lk>
2023-08-08 02:52:36 +0530

    add signup3

--- src/bank/Signup3.java
+++ src/bank/Signup3.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class Signup3 extends JFrame {
 
+    JRadioButton r1,r2,r3,r4;
+
     Signup3(){
 
         ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
@@ -23,6 +25,36 @@ public class Signup3 extends JFrame {
         l2.setFont(new Font("Railway",Font.BOLD,22));
         l2.setBounds(280,70,400,40);
         add(l2);
+
+        JLabel l3 = new JLabel("Account Type:");
+        l2.setFont(new Font("Railway",Font.BOLD,18));
+        l2.setBounds(100,140,200,30);
+        add(l3);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Railway",Font.BOLD,16));
+        r1.setBackground(new Color(215,252,252));
+        r1.setBounds(100,180,150,30);
+        add(r1);
+
+        r2 = new JRadioButton("FIxed Deposit Account");
+        r2.setFont(new Font("Railway",Font.BOLD,16));
+        r2.setBackground(new Color(215,252,252));
+        r2.setBounds(350,180,300,30);
+        add(r2);
+
+        r3 = new JRadioButton("Current Account");
+        r3.setFont(new Font("Railway",Font.BOLD,16));
+        r3.setBackground(new Color(215,252,252));
+        r3.setBounds(100,220,250,30);
+        add(r3);
+
+        r4 = new JRadioButton("Recurring Deposit Account");
+        r4.setFont(new Font("Railway",Font.BOLD,16));
+        r4.setBackground(new Color(215,252,252));
+        r4.setBounds(350,220,250,30);
+        add(r4);
+
 
 
         getContentPane().setBackground(new Color(215,252,252));
