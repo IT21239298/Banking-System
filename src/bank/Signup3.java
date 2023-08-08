08ca083881f1785e2950c2bea2f8e6a783efb577
IT21239298 <it21239298@my.sliit.lk>
2023-08-08 16:40:52 +0530

    add signup3 -checkbox

--- src/bank/Signup3.java
+++ src/bank/Signup3.java
@@ -7,6 +7,8 @@ public class Signup3 extends JFrame {
 
     JRadioButton r1,r2,r3,r4;
 
+    JCheckBox c1,c2,c3,c4,c5,c6;
+
     Signup3(){
 
         ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
@@ -27,8 +29,8 @@ public class Signup3 extends JFrame {
         add(l2);
 
         JLabel l3 = new JLabel("Account Type:");
-        l2.setFont(new Font("Railway",Font.BOLD,18));
-        l2.setBounds(100,140,200,30);
+        l3.setFont(new Font("Railway",Font.BOLD,18));
+        l3.setBounds(100,140,200,30);
         add(l3);
 
         r1 = new JRadioButton("Saving Account");
@@ -54,6 +56,102 @@ public class Signup3 extends JFrame {
         r4.setBackground(new Color(215,252,252));
         r4.setBounds(350,220,250,30);
         add(r4);
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(r1);
+        buttonGroup.add(r2);
+        buttonGroup.add(r3);
+        buttonGroup.add(r4);
+
+        JLabel l4 = new JLabel("Card Number:");
+        l4.setFont(new Font("Railway",Font.BOLD,18));
+        l4.setBounds(100,300,200,30);
+        add(l4);
+
+        JLabel l5 = new JLabel("(Your 16-digit Card Number)");
+        l5.setFont(new Font("Railway",Font.BOLD,12));
+        l5.setBounds(100,330,200,20);
+        add(l5);
+
+        JLabel l6 = new JLabel("xxxx-xxxx-xxxx-4528");
+        l6.setFont(new Font("Railway",Font.BOLD,18));
+        l6.setBounds(330,300,250,30);
+        add(l6);
+
+        JLabel l7 = new JLabel("(It would appear on atm card/cheque Book and Statements)");
+        l7.setFont(new Font("Railway",Font.BOLD,12));
+        l7.setBounds(330,330,500,20);
+        add(l7);
+
+        JLabel l8 = new JLabel("PIN :");
+        l8.setFont(new Font("Railway",Font.BOLD,18));
+        l8.setBounds(100,370,200,30);
+        add(l8);
+
+        JLabel l9 = new JLabel("xxxx");
+        l9.setFont(new Font("Railway",Font.BOLD,18));
+        l9.setBounds(330,370,200,30);
+        add(l9);
+
+        JLabel l10 = new JLabel("(4-digit Password)");
+        l10.setFont(new Font("Railway",Font.BOLD,12));
+        l10.setBounds(100,400,200,20);
+        add(l10);
+
+        JLabel l11 = new JLabel("Services Required :");
+        l11.setFont(new Font("Railway",Font.BOLD,18));
+        l11.setBounds(100,450,200,30);
+        add(l11);
+
+        c1 = new JCheckBox("ATM CARD");
+        c1.setBackground(new Color(215,252,252));
+        c1.setFont(new Font("Railway",Font.BOLD,16));
+        c1.setBounds(100,500,200,30);
+        add(c1);
+
+        c2 = new JCheckBox("Internet Banking");
+        c2.setBackground(new Color(215,252,252));
+        c2.setFont(new Font("Railway",Font.BOLD,16));
+        c2.setBounds(350,500,200,30);
+        add(c2);
+
+        c3 = new JCheckBox("Mobile Banking");
+        c3.setBackground(new Color(215,252,252));
+        c3.setFont(new Font("Railway",Font.BOLD,16));
+        c3.setBounds(100,550,200,30);
+        add(c3);
+
+        c4 = new JCheckBox("Email Alerts");
+        c4.setBackground(new Color(215,252,252));
+        c4.setFont(new Font("Railway",Font.BOLD,16));
+        c4.setBounds(350,550,200,30);
+        add(c4);
+
+        c5 = new JCheckBox("Cheque Book");
+        c5.setBackground(new Color(215,252,252));
+        c5.setFont(new Font("Railway",Font.BOLD,16));
+        c5.setBounds(100,600,200,30);
+        add(c5);
+
+        c6 = new JCheckBox("E-Statement");
+        c6.setBackground(new Color(215,252,252));
+        c6.setFont(new Font("Railway",Font.BOLD,16));
+        c6.setBounds(350,600,200,30);
+        add(c6);
+
+        JCheckBox c7 = new JCheckBox("I here by declares that the above entered details correct to the best of my knowledge.",true);
+        c7.setBackground(new Color(215,252,252));
+        c7.setFont(new Font("Railway",Font.BOLD,12));
+        c7.setBounds(100,680,600,20);
+        add(c7);
+
+
+
+
+
+
+
+
 
 
 
