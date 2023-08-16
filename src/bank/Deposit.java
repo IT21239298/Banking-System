965ed6220e88ad5cc6c0e896ce68329760ad905f
IT21239298 <it21239298@my.sliit.lk>
2023-08-17 02:57:01 +0530

    create deposit class

--- src/bank/Deposit.java
+++ src/bank/Deposit.java
@@ -1,4 +1,21 @@
 package bank;
 
-public class Deposit {
+import javax.swing.*;
+
+public class Deposit extends JFrame {
+    String pin;
+
+    Deposit(String pin){
+        this.pin = pin;
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
+        //ssss
+
+    }
+    public static void main(String[] args) {
+        new Deposit("");
+    }
+
 }
