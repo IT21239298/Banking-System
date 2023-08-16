6757d498f9530a1f50433434e9f9b6d9746f5860
IT21239298 <it21239298@my.sliit.lk>
2023-08-17 03:04:47 +0530

    create deposit background

--- src/bank/Deposit.java
+++ src/bank/Deposit.java
@@ -1,14 +1,23 @@
 package bank;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Deposit extends JFrame {
     String pin;
 
     Deposit(String pin){
         this.pin = pin;
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
+        ImageIcon i3  = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0,0,1550,830);
+        add(l3);
+
+
         setLayout(null);
-        setSize(1550,1080);
+        setSize(1550,830);
         setLocation(0,0);
         setVisible(true);
         //ssss
