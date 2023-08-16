2813bfb6189d1a2a40d4627b49ed812a4cd0870d
IT21239298 <it21239298@my.sliit.lk>
2023-08-17 03:59:01 +0530

    crete databse connection in deposit

--- src/bank/Deposit.java
+++ src/bank/Deposit.java
@@ -2,9 +2,16 @@ package bank;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
-public class Deposit extends JFrame {
+public class Deposit extends JFrame  implements ActionListener {
     String pin;
+
+    TextField textField;
+
+    JButton b1,b2;
 
     Deposit(String pin){
         this.pin = pin;
@@ -15,6 +22,33 @@ public class Deposit extends JFrame {
         l3.setBounds(0,0,1550,830);
         add(l3);
 
+        JLabel label1 = new JLabel("ENTER AMONT WANT TO DEPOSIT");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("System",Font.BOLD,16));
+        label1.setBounds(460,180,400,35);
+        l3.add(label1);
+
+        textField = new TextField();
+        textField.setBackground(new Color(65,125,128));
+        textField.setForeground(Color.WHITE);
+        textField.setBounds(460,230,320,25);
+        textField.setFont(new Font("Railway", Font.BOLD,22));
+        l3.add(textField);
+
+        b1 = new JButton("DEPOSIT");
+        b1.setBounds(700,352,150,35);
+        b1.setBackground(new Color(65,125,128));
+        b1.setForeground(Color.WHITE);
+       // b1.addActionListener(this);
+        l3.add(b1);
+
+        b2 = new JButton("BACK");
+        b2.setBounds(700,406,150,35);
+        b2.setBackground(new Color(65,125,128));
+        b2.setForeground(Color.WHITE);
+        // b2.addActionListener(this);
+        l3.add(b2);
+
 
         setLayout(null);
         setSize(1550,830);
@@ -23,6 +57,20 @@ public class Deposit extends JFrame {
         //ssss
 
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            String amount = textField.getText();
+            Date date = new Date();
+            if (e.getSource()==b1){
+
+            }
+        }catch (Exception E){
+            E.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         new Deposit("");
     }
