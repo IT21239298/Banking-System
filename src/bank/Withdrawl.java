8f9bd5b842f66c396d8ce4a0682219ef1656be14
IT21239298 <it21239298@my.sliit.lk>
2023-08-18 02:16:59 +0530

    add withdrawl

--- src/bank/Withdrawl.java
+++ src/bank/Withdrawl.java
@@ -2,20 +2,15 @@ package bank;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Date;
 
-public class Deposit extends JFrame  implements ActionListener {
-    String pin;
+public class Withdrawl extends JFrame {
+
+
 
     TextField textField;
 
     JButton b1,b2;
-
-    Deposit(String pin){
-
-        this.pin = pin;
+    Withdrawl(){
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
         Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
@@ -41,14 +36,14 @@ public class Deposit extends JFrame  implements ActionListener {
         b1.setBounds(700,352,150,35);
         b1.setBackground(new Color(65,125,128));
         b1.setForeground(Color.WHITE);
-        b1.addActionListener(this);
+       // b1.addActionListener(this);
         l3.add(b1);
 
         b2 = new JButton("BACK");
         b2.setBounds(700,406,150,35);
         b2.setBackground(new Color(65,125,128));
         b2.setForeground(Color.WHITE);
-        b2.addActionListener(this);
+        //b2.addActionListener(this);
         l3.add(b2);
 
 
@@ -60,32 +55,9 @@ public class Deposit extends JFrame  implements ActionListener {
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        try {
-            String amount = textField.getText();
-            Date date = new Date();
-            if (e.getSource()==b1){
-                if (textField.getText().equals("")){
-                    JOptionPane.showMessageDialog(null,"Please enter the Amount you want to deposit");
-                }else{
-                    Con c = new Con();
-                    c.statement.executeUpdate("insert into bank values ('"+pin+"','"+date+"','Deposit','"+amount+"')");
-                    JOptionPane.showMessageDialog(null,"Rs."+amount+" Deposit SuccessFully..!");
-                    setVisible(false);
-                }
-
-            }else if (e.getSource()==b2){
-                setVisible(false);
-
-            }
-        }catch (Exception E){
-            E.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
-        new Deposit("");
-    }
+        new Withdrawl();
 
+    }
 }
--- src/bank/Wtihdrawl.java
+++
@@ -1,4 +0,0 @@
-package bank;
-
-public class Wtihdrawl {
-}
