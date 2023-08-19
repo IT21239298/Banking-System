218a92c8d001f2e8ef8310e25ba11d2644e09d44
IT21239298 <it21239298@my.sliit.lk>
2023-08-20 00:24:31 +0530

    add action Lisiner

--- src/bank/main_Class.java
+++ src/bank/main_Class.java
@@ -2,8 +2,10 @@ package bank;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class main_Class extends JFrame {
+public class main_Class extends JFrame implements ActionListener {
 
     JButton b1,b2,b3,b4,b5,b6,b7;
     main_Class(){
@@ -25,6 +27,7 @@ public class main_Class extends JFrame {
         b1.setForeground(Color.WHITE);
         b1.setBackground(new Color(65,125,128));
         b1.setBounds(410,272,150,35);
+        b1.addActionListener(this);
         l3.add(b1);
 
 
@@ -32,36 +35,42 @@ public class main_Class extends JFrame {
         b2.setForeground(Color.WHITE);
         b2.setBackground(new Color(65,125,128));
         b2.setBounds(700,274,150,35);
+        b2.addActionListener(this);
         l3.add(b2);
 
         b3 =  new JButton("FAST CASH");
         b3.setForeground(Color.WHITE);
         b3.setBackground(new Color(65,125,128));
         b3.setBounds(410,318,150,35);
+        b3.addActionListener(this);
         l3.add(b3);
 
         b4 =  new JButton("MINI STATEMENT");
         b4.setForeground(Color.WHITE);
         b4.setBackground(new Color(65,125,128));
         b4.setBounds(700,318,150,35);
+        b4.addActionListener(this);
         l3.add(b4);
 
         b5 =  new JButton("PIN CHANGE");
         b5.setForeground(Color.WHITE);
         b5.setBackground(new Color(65,125,128));
         b5.setBounds(410,362,150,35);
+        b5.addActionListener(this);
         l3.add(b5);
 
         b6 =  new JButton("BALANCE ENQUIRY");
         b6.setForeground(Color.WHITE);
         b6.setBackground(new Color(65,125,128));
         b6.setBounds(700,362,150,35);
+        b6.addActionListener(this);
         l3.add(b6);
 
         b7 =  new JButton("EXIT");
         b7.setForeground(Color.WHITE);
         b7.setBackground(new Color(65,125,128));
         b7.setBounds(700,406,150,35);
+        b7.addActionListener(this);
         l3.add(b7);
 
 
@@ -72,6 +81,11 @@ public class main_Class extends JFrame {
         setSize(1550,1080);
         setLocation(0,0);
         setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 
