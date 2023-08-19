84c699a256566d9755a41d77341235c49a36cff4
IT21239298 <it21239298@my.sliit.lk>
2023-08-19 10:18:08 +0530

    create main_class button last withdrawl

--- src/bank/main_Class.java
+++ src/bank/main_Class.java
@@ -1,11 +1,51 @@
 package bank;
 
-public class main_Class {
+import javax.swing.*;
+import java.awt.*;
+
+public class main_Class extends JFrame {
+
+    JButton b1,b2,b3,b4,b5,b6,b7;
     main_Class(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
+        ImageIcon i3  = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0,0,1550,830);
+        add(l3);
+
+        JLabel label = new JLabel("Please Select Your Transaction");
+        label.setBounds(430,180,700,35);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("System",Font.BOLD,28));
+        l3.add(label);
+
+        b1 =  new JButton("DEPOSIT");
+        b1.setForeground(Color.WHITE);
+        b1.setBackground(new Color(65,125,128));
+        b1.setBounds(410,272,150,35);
+        l3.add(b1);
+
+
+
+
+        // meaka hadanna oneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        b2 =  new JButton("CASH WITHDRAWL");
+        b2.setForeground(Color.WHITE);
+        b2.setBackground(new Color(65,125,128));
+        b2.setBounds(410,272,150,35);
+        l3.add(b2);
+
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
 
     }
 
     public static void main(String[] args) {
+        new main_Class();
 
     }
 }
