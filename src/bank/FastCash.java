ece751e55de7578be4e94bab55632c2c74b5bdba
IT21239298 <it21239298@my.sliit.lk>
2023-08-22 18:37:05 +0530

    change name in button

--- src/bank/FastCash.java
+++ src/bank/FastCash.java
@@ -17,55 +17,55 @@ public class FastCash extends JFrame  implements ActionListener {
         l3.setBounds(0,0,1550,830);
         add(l3);
 
-        JLabel label = new JLabel("Please Select Your Transaction");
+        JLabel label = new JLabel("SELECT WITHDRAWAL AMOUNT");
         label.setBounds(430,180,700,35);
         label.setForeground(Color.WHITE);
         label.setFont(new Font("System",Font.BOLD,28));
         l3.add(label);
 
-        b1 =  new JButton("DEPOSIT");
+        b1 =  new JButton("Rs.100");
         b1.setForeground(Color.WHITE);
         b1.setBackground(new Color(65,125,128));
         b1.setBounds(410,272,150,35);
         b1.addActionListener(this);
         l3.add(b1);
 
-        b2 =  new JButton("CASH WITHDRAWL");
+        b2 =  new JButton("Rs.500");
         b2.setForeground(Color.WHITE);
         b2.setBackground(new Color(65,125,128));
         b2.setBounds(700,274,150,35);
         b2.addActionListener(this);
         l3.add(b2);
 
-        b3 =  new JButton("FAST CASH");
+        b3 =  new JButton("Rs.1000");
         b3.setForeground(Color.WHITE);
         b3.setBackground(new Color(65,125,128));
         b3.setBounds(410,318,150,35);
         b3.addActionListener(this);
         l3.add(b3);
 
-        b4 =  new JButton("MINI STATEMENT");
+        b4 =  new JButton("Rs.2000");
         b4.setForeground(Color.WHITE);
         b4.setBackground(new Color(65,125,128));
         b4.setBounds(700,318,150,35);
         b4.addActionListener(this);
         l3.add(b4);
 
-        b5 =  new JButton("PIN CHANGE");
+        b5 =  new JButton("Rs.5000");
         b5.setForeground(Color.WHITE);
         b5.setBackground(new Color(65,125,128));
         b5.setBounds(410,362,150,35);
         b5.addActionListener(this);
         l3.add(b5);
 
-        b6 =  new JButton("BALANCE ENQUIRY");
+        b6 =  new JButton("Rs.10000");
         b6.setForeground(Color.WHITE);
         b6.setBackground(new Color(65,125,128));
         b6.setBounds(700,362,150,35);
         b6.addActionListener(this);
         l3.add(b6);
 
-        b7 =  new JButton("EXIT");
+        b7 =  new JButton("BACK");
         b7.setForeground(Color.WHITE);
         b7.setBackground(new Color(65,125,128));
         b7.setBounds(700,406,150,35);
